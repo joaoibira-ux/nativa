3950cfa5dcef3fb1e03a556511176f1e9284415c
joaoibira-ux <joao.ibira@gmail.com>
2026-06-13 00:43:49 -0300

    v1.5: campo UD de materia-prima/produtos passa a ser select com Kg, Gr, Ud

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const VERSAO_NATIVA = "1.4";
+const VERSAO_NATIVA = "1.5";
 
 document.addEventListener("DOMContentLoaded", () => {
   const el = document.getElementById("versao-app");
--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "nativa-v5";
+const VERSION = "nativa-v6";
 const ASSETS = [
   "./index.html",
   "./menu.html",
@@ -6,8 +6,8 @@ const ASSETS = [
   "./materiaprima.html",
   "./produtos.html",
   "./pedidos.html",
-  "./style.css?v=4",
-  "./app.js?v=5",
+  "./style.css?v=5",
+  "./app.js?v=6",
   "./clientes.js?v=1",
   "./estoque.js?v=1",
   "./pedidos.js?v=1",
